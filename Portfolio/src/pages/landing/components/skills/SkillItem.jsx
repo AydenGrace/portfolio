60ab8e5fb168b4a8dfb4598058b24bb039686273
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-05-13 14:34:58 +0200

    skills img

--- Portfolio/src/pages/landing/components/skills/SkillItem.jsx
+++ Portfolio/src/pages/landing/components/skills/SkillItem.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import style from "./SkillItem.module.scss";
-
 export default function SkillItem({ skill }) {
   return (
-    <div className={`${style.container}`}>
-      <img src={skill.icon} alt={skill.title} height={"40px"} />
-      <p>{skill.title}</p>
+    <div
+      className={`w-[260px] h-[50px] rounded-xl mt-2.5 flex items-center justify-center bg-center bg-cover`}
+      style={{ backgroundImage: `url("${skill.background}")` }}
+    >
+      <div className="w-full flex items-center gap-2.5 bg-black/75 p-2.5 pl-11 cursor-default">
+        <img src={skill.icon} alt={skill.title} height={"40px"} />
+        <p>{skill.title}</p>
+      </div>
     </div>
   );
 }
