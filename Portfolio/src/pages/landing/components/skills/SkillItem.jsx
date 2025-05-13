5b4c55e2a670faf090ca488519d721fbd8abab27
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-05-13 16:14:16 +0200

    skill img correction'

--- Portfolio/src/pages/landing/components/skills/SkillItem.jsx
+++ Portfolio/src/pages/landing/components/skills/SkillItem.jsx
@@ -4,7 +4,7 @@ export default function SkillItem({ skill }) {
       className={`w-[260px] h-[50px] rounded-xl mt-2.5 flex items-center justify-center bg-center bg-cover`}
       style={{ backgroundImage: `url("${skill.background}")` }}
     >
-      <div className="w-full flex items-center gap-2.5 bg-black/75 p-2.5 pl-11 cursor-default">
+      <div className="w-full flex items-center gap-2.5 bg-black/80 p-2.5 pl-11 cursor-default">
         <img src={skill.icon} alt={skill.title} height={"40px"} />
         <p>{skill.title}</p>
       </div>
